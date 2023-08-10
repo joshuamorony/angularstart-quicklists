00053377d231aa98b83ff114f5a9bdf0abb0aac8
Joshua Morony <joshua.morony@gmail.com>
2023-08-10 14:41:51 +0930

    feat: display no checklists message

--- src/app/home/ui/checklist-list.component.ts
+++ src/app/home/ui/checklist-list.component.ts
@@ -4,7 +4,11 @@ import { Checklist } from 'src/app/shared/interfaces/checklist';
 @Component({
   standalone: true,
   selector: 'app-checklist-list',
-  template: ` <p>Hello world</p> `,
+  template: `
+    <p data-testid="no-checklists-message">
+      Click the add button to add your first checklist!
+    </p>
+  `,
 })
 export class ChecklistListComponent {
   @Input({ required: true }) checklists!: Checklist[];

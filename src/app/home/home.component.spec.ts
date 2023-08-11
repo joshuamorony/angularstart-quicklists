66a8bed90e6f406cf17044341e99e41de6d1cb3d
Joshua Morony <joshua.morony@gmail.com>
2023-08-11 20:35:23 +0930

    test: app-form-modal set isOpen to true when add button clicked

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import HomeComponent from './home.component';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -22,5 +23,21 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('app-form-modal', () => {
+    it('should have isOpen set to true when add button clicked', () => {
+      const addButton = fixture.debugElement.query(
+        By.css('[data-testid="create-checklist-button"]')
+      );
+
+      addButton.nativeElement.click();
+
+      fixture.detectChanges();
+
+      const modal = fixture.debugElement.query(By.css('app-form-modal'));
+
+      expect(modal.componentInstance.isOpen).toBe(true);
+    });
   });
 });

ce4e7430b600f5e9c249deaeee1fd54f8f178447
Joshua Morony <joshua.morony@gmail.com>
2023-08-15 12:05:18 +0930

    test: add$ source nexted when form saved

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -3,20 +3,35 @@ import HomeComponent from './home.component';
 import { By } from '@angular/platform-browser';
 import { ModalComponent } from '../shared/ui/modal.component';
 import { MockModalComponent } from '../shared/ui/modal.component.spec';
+import { ChecklistService } from '../shared/data-access/checklist.service';
+import { DebugElement } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let checklistService: ChecklistService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [
+        {
+          provide: ChecklistService,
+          useValue: {
+            add$: {
+              next: jest.fn(),
+            },
+          },
+        },
+      ],
     })
       .overrideComponent(HomeComponent, {
         remove: { imports: [ModalComponent] },
         add: { imports: [MockModalComponent] },
       })
       .compileComponents();
+
+    checklistService = TestBed.inject(ChecklistService);
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -28,18 +43,35 @@ describe('HomeComponent', () => {
   });
 
   describe('app-modal', () => {
-    it('should have isOpen set to true when add button clicked', () => {
-      const addButton = fixture.debugElement.query(
-        By.css('[data-testid="create-checklist-button"]')
-      );
+    let appModal: DebugElement;
 
-      addButton.nativeElement.click();
+    beforeEach(() => {
+      appModal = fixture.debugElement.query(By.css('app-modal'));
+    });
 
-      fixture.detectChanges();
+    describe('input: isOpen', () => {
+      it('should be set to true when add button clicked', () => {
+        const addButton = fixture.debugElement.query(
+          By.css('[data-testid="create-checklist-button"]')
+        );
 
-      const modal = fixture.debugElement.query(By.css('app-modal'));
+        addButton.nativeElement.click();
 
-      expect(modal.componentInstance.isOpen).toBeTruthy();
+        fixture.detectChanges();
+
+        const modal = fixture.debugElement.query(By.css('app-modal'));
+
+        expect(modal.componentInstance.isOpen).toBeTruthy();
+      });
+    });
+
+    describe('output: save', () => {
+      it('should next add$ source with form values', () => {
+        appModal.triggerEventHandler('save');
+        expect(checklistService.add$.next).toHaveBeenCalledWith(
+          component.checklistForm.getRawValue()
+        );
+      });
     });
   });
 });

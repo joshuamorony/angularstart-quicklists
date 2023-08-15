8f998b0e1045413c2c2b81b623450825bed0f097
Joshua Morony <joshua.morony@gmail.com>
2023-08-15 14:43:17 +0930

    feat: add checklists to state when add$ emits

--- src/app/shared/data-access/checklist.service.ts
+++ src/app/shared/data-access/checklist.service.ts
@@ -1,11 +1,57 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, computed, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Subject } from 'rxjs';
+import { AddChecklist, Checklist } from '../interfaces/checklist';
+
+export interface ChecklistsState {
+  checklists: Checklist[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChecklistService {
-  checklists = signal<{ title: string }[]>([]);
+  // state
+  private state = signal<ChecklistsState>({
+    checklists: [],
+  });
 
-  add$ = new Subject();
+  // selectors
+  checklists = computed(() => this.state().checklists);
+
+  // sources
+  add$ = new Subject<AddChecklist>();
+
+  constructor() {
+    this.add$.pipe(takeUntilDestroyed()).subscribe((checklist) =>
+      this.state.update((state) => ({
+        ...state,
+        checklists: [...state.checklists, this.addIdToChecklist(checklist)],
+      }))
+    );
+  }
+
+  private addIdToChecklist(checklist: AddChecklist) {
+    return {
+      ...checklist,
+      id: this.generateSlug(checklist.title),
+    };
+  }
+
+  private generateSlug(title: string) {
+    // NOTE: This is a simplistic slug generator and will not handle things like special characters.
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+
+    // Check if the slug already exists
+    const matchingSlugs = this.checklists().find(
+      (checklist) => checklist.id === slug
+    );
+
+    // If the title is already being used, add a string to make the slug unique
+    if (matchingSlugs) {
+      slug = slug + Date.now().toString();
+    }
+
+    return slug;
+  }
 }

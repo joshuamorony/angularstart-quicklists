1e0c06b8c9ccb754c09e53a8bea460568f99d6ad
Joshua Morony <joshua.morony@gmail.com>
2023-08-24 13:41:32 +0930

    feat: supply checklist matching param as input

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ChecklistHeaderComponent } from './ui/checklist-header.component';
+import { ChecklistService } from '../shared/data-access/checklist.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-checklist',
-  template: ` <p>Hello world</p> `,
+  template: `
+    <app-checklist-header
+      *ngIf="checklist() as checklist"
+      [checklist]="checklist"
+    />
+  `,
+  imports: [CommonModule, ChecklistHeaderComponent],
 })
-export default class ChecklistComponent {}
+export default class ChecklistComponent {
+  checklistService = inject(ChecklistService);
+  route = inject(ActivatedRoute);
+
+  params = toSignal(this.route.paramMap);
+
+  checklist = computed(() =>
+    this.checklistService
+      .checklists()
+      .find((checklist) => checklist.id === this.params()?.get('id'))
+  );
+}

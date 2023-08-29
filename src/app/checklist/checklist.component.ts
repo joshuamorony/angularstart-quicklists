b900eb590d806db92f3068b6f1e8cb55aabab6d2
Joshua Morony <joshua.morony@gmail.com>
2023-08-29 10:32:34 +0930

    feat: set checklistIdBeingEdited when addItem emits

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,10 +1,12 @@
-import { Component, computed, effect, inject } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ChecklistHeaderComponent } from './ui/checklist-header.component';
 import { ChecklistService } from '../shared/data-access/checklist.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ChecklistItemListComponent } from './ui/checklist-item-list.component';
+import { ModalComponent } from '../shared/ui/modal.component';
+import { ChecklistItem } from '../shared/interfaces/checklist-item';
 
 @Component({
   standalone: true,
@@ -13,15 +15,27 @@ import { ChecklistItemListComponent } from './ui/checklist-item-list.component';
     <app-checklist-header
       *ngIf="checklist() as checklist"
       [checklist]="checklist"
+      (addItem)="checklistItemBeingEdited.set({})"
     />
 
     <app-checklist-item-list [checklistItems]="[]" />
+
+    <app-modal [isOpen]="!!checklistItemBeingEdited()">
+      <ng-template> </ng-template>
+    </app-modal>
   `,
-  imports: [CommonModule, ChecklistHeaderComponent, ChecklistItemListComponent],
+  imports: [
+    CommonModule,
+    ChecklistHeaderComponent,
+    ChecklistItemListComponent,
+    ModalComponent,
+  ],
 })
 export default class ChecklistComponent {
   checklistService = inject(ChecklistService);
   route = inject(ActivatedRoute);
+
+  checklistItemBeingEdited = signal<Partial<ChecklistItem> | null>(null);
 
   params = toSignal(this.route.paramMap);
 

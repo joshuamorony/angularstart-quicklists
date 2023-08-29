0b7758472b359498e0c0890b7422e7d59be1bf49
Joshua Morony <joshua.morony@gmail.com>
2023-08-29 11:32:00 +0930

    feat: add data from add$ source to checklist items state

--- src/app/checklist/data-access/checklist-item.service.ts
+++ src/app/checklist/data-access/checklist-item.service.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, computed, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Subject } from 'rxjs';
-import { AddChecklistItem } from 'src/app/shared/interfaces/checklist-item';
+import {
+  AddChecklistItem,
+  ChecklistItem,
+} from 'src/app/shared/interfaces/checklist-item';
+
+export interface ChecklistItemsState {
+  checklistItems: ChecklistItem[];
+  loaded: boolean;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChecklistItemService {
+  // state
+  private state = signal<ChecklistItemsState>({
+    checklistItems: [],
+    loaded: false,
+  });
+
+  // selectors
+  checklistItems = computed(() => this.state().checklistItems);
+
   // sources
   add$ = new Subject<AddChecklistItem>();
+
+  constructor() {
+    this.add$.pipe(takeUntilDestroyed()).subscribe((checklistItem) =>
+      this.state.update((state) => ({
+        ...state,
+        checklistItems: [
+          ...state.checklistItems,
+          {
+            id: Date.now().toString(),
+            checklistId: checklistItem.checklistId,
+            checked: false,
+            ...checklistItem.item,
+          },
+        ],
+      }))
+    );
+  }
 }

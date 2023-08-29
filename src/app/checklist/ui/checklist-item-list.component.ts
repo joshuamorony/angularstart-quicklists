f9670634bb09d387edeef91a1e83cfd34ce04446
Joshua Morony <joshua.morony@gmail.com>
2023-08-29 09:34:32 +0930

    feat: no checklist items message

--- src/app/checklist/ui/checklist-item-list.component.ts
+++ src/app/checklist/ui/checklist-item-list.component.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ChecklistItem } from 'src/app/shared/interfaces/checklist-item';
 
 @Component({
   standalone: true,
   selector: 'app-checklist-item-list',
-  template: ` <p>Hello world</p> `,
+  template: `
+    <div *ngIf="!checklistItems.length">
+      <h2>Add an item</h2>
+      <p data-testid="no-checklist-items-message">
+        Click the add button to add your first item to this quicklist
+      </p>
+    </div>
+  `,
+  imports: [CommonModule],
 })
 export class ChecklistItemListComponent {
   @Input({ required: true }) checklistItems!: ChecklistItem[];

ed01a62f20067c59204a1a29e59833cfaa73dcbf
Joshua Morony <joshua.morony@gmail.com>
2023-08-29 11:35:15 +0930

    feat: render item for each checklist item input

--- src/app/checklist/ui/checklist-item-list.component.ts
+++ src/app/checklist/ui/checklist-item-list.component.ts
@@ -6,6 +6,15 @@ import { ChecklistItem } from 'src/app/shared/interfaces/checklist-item';
   standalone: true,
   selector: 'app-checklist-item-list',
   template: `
+    <ul>
+      <li
+        *ngFor="let item of checklistItems; trackBy: trackByFn"
+        data-testid="checklist-item"
+      >
+        {{ item.title }}
+      </li>
+    </ul>
+
     <div *ngIf="!checklistItems.length">
       <h2>Add an item</h2>
       <p data-testid="no-checklist-items-message">
@@ -17,4 +26,8 @@ import { ChecklistItem } from 'src/app/shared/interfaces/checklist-item';
 })
 export class ChecklistItemListComponent {
   @Input({ required: true }) checklistItems!: ChecklistItem[];
+
+  trackByFn(index: number, item: ChecklistItem) {
+    return item.id;
+  }
 }

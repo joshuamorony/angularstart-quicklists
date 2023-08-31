d0ca580d3853581169e11f3f2deb7233743f3604
Joshua Morony <joshua.morony@gmail.com>
2023-09-01 09:17:30 +0930

    feat: loadChecklists returns observable of checklists storage

--- src/app/shared/data-access/storage.service.ts
+++ src/app/shared/data-access/storage.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, InjectionToken, PLATFORM_ID, inject } from '@angular/core';
 import { of } from 'rxjs';
+import { Checklist } from '../interfaces/checklist';
+
+export const LOCAL_STORAGE = new InjectionToken<Storage>(
+  'window local storage object',
+  {
+    providedIn: 'root',
+    factory: () => {
+      return inject(PLATFORM_ID) === 'browser'
+        ? window.localStorage
+        : ({} as Storage);
+    },
+  }
+);
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
+  storage = inject(LOCAL_STORAGE);
+
   loadChecklists() {
-    return of();
+    const checklists = this.storage.getItem('checklists');
+    return of(JSON.parse(checklists!) as Checklist[]);
   }
 }

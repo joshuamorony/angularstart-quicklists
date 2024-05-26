4ea6e03cd48400b11e437dac8e32534a198f2e62
Joshua Morony <joshua.morony@gmail.com>
2024-05-26 12:59:35 +0930

    refactor: form modal component

--- src/app/shared/ui/form-modal.component.ts
+++ src/app/shared/ui/form-modal.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -7,20 +7,20 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
   selector: 'app-form-modal',
   template: `
     <header>
-      <h2>{{ title }}</h2>
+      <h2>{{ title() }}</h2>
       <button (click)="close.emit()">close</button>
     </header>
     <section>
-      <form [formGroup]="formGroup" (ngSubmit)="save.emit(); close.emit()">
-        @for (control of formGroup.controls | keyvalue; track control.key){
-        <div>
-          <label [for]="control.key">{{ control.key }}</label>
-          <input
-            [id]="control.key"
-            type="text"
-            [formControlName]="control.key"
-          />
-        </div>
+      <form [formGroup]="formGroup()" (ngSubmit)="save.emit(); close.emit()">
+        @for (control of formGroup().controls | keyvalue; track control.key) {
+          <div>
+            <label [for]="control.key">{{ control.key }}</label>
+            <input
+              [id]="control.key"
+              type="text"
+              [formControlName]="control.key"
+            />
+          </div>
         }
         <button type="submit" data-testid="save-checklist-button">Save</button>
       </form>
@@ -56,8 +56,8 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
   imports: [ReactiveFormsModule, CommonModule],
 })
 export class FormModalComponent {
-  @Input({ required: true }) formGroup!: FormGroup;
-  @Input({ required: true }) title!: string;
-  @Output() save = new EventEmitter<void>();
-  @Output() close = new EventEmitter<void>();
+  formGroup = input.required<FormGroup>();
+  title = input.required<string>();
+  save = output();
+  close = output();
 }
